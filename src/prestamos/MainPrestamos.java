680a82298a0427093f4560c99ba101237e1c5a02
AWO <@isaacolajim>
2026-02-13 09:19:46 +0100

    Primera opcion del Main

--- src/prestamos/MainPrestamos.java
+++ src/prestamos/MainPrestamos.java
@@ -1,15 +1,72 @@
 package prestamos;
 
 import java.time.LocalDate;
-
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
 public class MainPrestamos {
-    public static void main(String[] args)throws Excepciones.UsuarioInvalidoException {
-        Usuario u1 = new Usuario("Isaac","isaac@gmail.com","SOC00123",
-                LocalDate.of(2000,12,12));
-        u1.sancionar();
-        System.out.println(u1.toString());
-        System.out.println(u1.estaSancionado());
-        u1.levantarSancion();
-        System.out.println(u1.estaSancionado());
+    public static void main(String[] args)throws Excepciones.UsuarioInvalidoException, Excepciones.LibroNoDisponibleException,
+            Excepciones.PrestamoInvalidoException, Excepciones.UsuarioRepetidoException, Excepciones.UsuarioSancionadoException{
+        Scanner in = new Scanner(System.in);
+        GestorBiblioteca gestor = new GestorBiblioteca();
+        DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        int resp;
+        do{
+            System.out.println("1. REGISTRAR NUEVO USUARIO");
+            System.out.println("2. REALIZAR PRESTAMO DE LIBRO");
+            System.out.println("3. DEVOLVER LIBRO");
+            System.out.println("4. CONSULTAR ESTADO DE USUARIO");
+            System.out.println("5. MOSTRAR PRESTAMOS ACTIVOS");
+            System.out.println("6. MOSTRAR USUARIOS SANCIONADOS");
+            System.out.println("7. ACTUALIZAR SANCIONES");
+            System.out.println("8. SALIR");
+            try {
+                resp = Integer.parseInt(in.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Debes introducir un número.");
+                resp = 0; // Para que repita el bucle
+            }
+            switch (resp){
+                case 1:
+                    try{
+                        System.out.println("Nombre: ");
+                        String nombre = in.nextLine();
+                        System.out.print("Email: ");
+                        String email = in.nextLine();
+                        System.out.print("Número de socio (SOCxxxxx): ");
+                        String socio = in.nextLine();
+                        System.out.print("Fecha registro (dd/mm/aaaa): ");
+                        LocalDate fecha = LocalDate.parse(in.nextLine(), formatoFecha);
+
+                        Usuario u = new Usuario(nombre,email,socio,fecha);
+                        gestor.registrarUsuario(u);
+                        System.out.println("USUARIO CORRECTAMENRE REGISTRADO");
+                    }
+                    catch (DateTimeParseException e) {
+                        System.out.println("Error: Formato de fecha incorrecto. Usa dd/mm/aaaa");
+                    } catch (Exception e) {
+                        System.out.println("ERROR: " + e.getMessage());
+                    }
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+
+            }
+        }
+        while (resp!=8);
+
+
+
     }
 }
